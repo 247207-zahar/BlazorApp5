bb187a7956a5d6b807365ce94db0d62638a945e0
247207-zahra <247207@students.au.edu.pk>
2026-05-17 11:01:13 +0500

    end of cartservice

--- Services/CartService.cs
+++ Services/CartService.cs
@@ -32,3 +32,13 @@ namespace BlazorApp5.Services
             }
             NotifyChange
 
+        public void ClearCart()
+        {
+            _items.Clear();
+            NotifyChange();
+        }
+
+        private void NotifyChange() => OnCartChanged?.Invoke();
+    }
+}
+
