0b2410e260d9733d3ed8519d0b2004fa96a98fd4
247207-zahra <247207@students.au.edu.pk>
2026-05-17 11:04:16 +0500

    dictionary in cart

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -3,3 +3,12 @@ using BlazorApp5.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
+namespace BlazorApp5.Services
+{
+    public class CartService
+    {
+        // Dictionary use kar rahe hain: Key = MenuItem, Value = Quantity (int)
+        private readonly Dictionary<MenuItem, int> _items = new();
+
+        public event Action? OnCartChanged;
